f484dfcd6a5aadad0d3f45505f0d8ce7fa620e61
wujingtao <601595686@qq.com>
2021-01-16 01:13:55 +0800

    同步

--- app_config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    accessKeyId: 'Kyo3WFzasdVU7ttN',                        // 您的AccessKey Id
-    secretAccessKey: 'B2bWwVasdBwyUQ6ANxxe3BwnrRfoqg',      // 您的AccessKey Secret
-    appkey: 'z67Uasdn0aC8byNK',                             // 您的appkey
-    recognition_results: path.resolve(__dirname, 'recognition_results'),    // 语音识别结果保存位置
-    transcode_audios: path.resolve(__dirname, 'transcode_audios'),          // 音频转码保存位置
-};
---
+++ tools/transcode.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs-extra');
+const readline = require('readline');
+const child_process = require('child_process');
+
+/**
+ * 将媒体文件转换成阿里云要求的格式与编码
+ * 单声道 16k采样率 MP3 文件
+ */
+
+/**
+* 输出文件夹路径
+*/
+const outputPath = path.resolve(__dirname, '../recognition_audios');
+fs.ensureDirSync(outputPath);
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.question('将媒体文件转换成阿里云要求的格式与编码\n\n' + '请输入媒体文件所在的目录地址：', answer => {
+    const inputPath = answer.trim();
+    if (inputPath == '') process.exit();
+
+    const files = fs.readdirSync(inputPath)
+        .filter(i => /.(wav|mpeg|mp3|mp4|webm|aac|aacp|ogg|flac|rm|rmvb|3gp|avi|mpg|mov|mkv)$/i.test(path.extname(i)))
+        .map(i => path.resolve(inputPath, i))
+        .filter(i => fs.statSync(i).isFile());
+
+    for (let index = 0; index < files.length; index++) {
+        const meta = path.parse(files[index]);
+        console.log(`\n开始转换 [${index} / ${files.length}]：${meta.base}\n`);
+
+        meta.ext = '.mp3';
+        meta.dir = outputPath;
+        delete meta.base;
+        delete meta.root;
+
+        child_process.execFileSync('ffmpeg', [
+            '-i', files[index],
+            '-ar', '16000', '-ac', '1',
+            path.format(meta)
+        ], { stdio: 'inherit' });
+    }
+
+    process.exit();
+});
